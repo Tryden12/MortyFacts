9a22e5939981fc89b8d148b48d5c4b563ab8bbf4
Tyler Ryden <tryden11@gmail.com>
2023-01-08 15:41:49 -0500

    initial retrofit setup test = successful

--- app/src/main/java/com/tryden/mortyfacts/MainActivity.kt
+++ app/src/main/java/com/tryden/mortyfacts/MainActivity.kt
@@ -2,10 +2,40 @@ package com.tryden.mortyfacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tryden.mortyfacts.Constants.BASE_URL
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+        val rickAppCompatActivity: RickyAndMortyService = retrofit.create(RickyAndMortyService::class.java)
+
+        rickAppCompatActivity.getCharacterById().enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.e(TAG, response.toString() )
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.e(TAG, t.message ?: "Null message" )
+            }
+        })
     }
+
+
 }
---
+++ app/src/main/java/com/tryden/mortyfacts/RickyAndMortyService.kt
@@ -0,0 +1,10 @@
+package com.tryden.mortyfacts
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface RickyAndMortyService {
+
+    @GET("character/2")
+    fun getCharacterById() : Call<Any>
+}
