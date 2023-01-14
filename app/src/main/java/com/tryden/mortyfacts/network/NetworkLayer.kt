78c297933e1ac81e85d5228a918fb3a91e6e67bc
Tyler Ryden <tryden11@gmail.com>
2023-01-14 11:13:31 -0500

    okhttp & chucker interceptors implemented successfully

--- app/src/main/java/com/tryden/mortyfacts/network/NetworkLayer.kt
+++ app/src/main/java/com/tryden/mortyfacts/network/NetworkLayer.kt
@@ -1,17 +1,24 @@
 package com.tryden.mortyfacts.network
 
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tryden.mortyfacts.MortyFactsApplication
 import com.tryden.mortyfacts.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object NetworkLayer {
+
     // Moshi
     val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     // Retrofit
     val retrofit: Retrofit = Retrofit.Builder()
+        .client(getLoggingHttpClient())
         .baseUrl(BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
@@ -23,4 +30,20 @@ object NetworkLayer {
 
     // Client
     val apiClient = ApiClient(rickyAndMortyService)
+
+    private fun getLoggingHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BASIC)
+        })
+
+        builder.addInterceptor(ChuckerInterceptor.Builder(MortyFactsApplication.context)
+            .collector(ChuckerCollector(MortyFactsApplication.context))
+            .maxContentLength(250000L)
+            .redactHeaders(emptySet())
+            .alwaysReadResponseBody(false)
+            .build())
+
+        return builder.build()
+    }
 }
