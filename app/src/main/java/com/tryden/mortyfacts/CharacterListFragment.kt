dde4bb4e31192bad4fcda1ec1f5e595037aa11b6
Tyler Ryden <tryden11@gmail.com>
2023-01-15 17:08:33 -0500

    single activity migration and jetpack navigation setup successfully

--- app/src/main/java/com/tryden/mortyfacts/CharacterListFragment.kt
+++ app/src/main/java/com/tryden/mortyfacts/CharacterListFragment.kt
@@ -5,8 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.airbnb.epoxy.EpoxyRecyclerView
+import com.tryden.mortyfacts.characters.CharacterListPagingEpoxyController
+import com.tryden.mortyfacts.characters.CharactersViewModel
 
 class CharacterListFragment : Fragment() {
+
+    private val epoxyController = CharacterListPagingEpoxyController(::onCharacterSelected)
+
+    private val viewModel: CharactersViewModel by lazy {
+        ViewModelProvider(this)[CharactersViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,12 +28,19 @@ class CharacterListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_character_list, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view?.postDelayed({
-            val navGraphActivity = activity as NavGraphActivity
-            navGraphActivity.navController.navigate(R.id.action_characterListFragment_to_characterDetailFragment)
-        }, 1_500)
+        viewModel.charactersPagedListLiveData.observe(viewLifecycleOwner) { pagedList ->
+            epoxyController.submitList(pagedList)
+        }
+        view.findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView).setController(epoxyController)
+    }
+
+    private fun onCharacterSelected(characterId: Int) {
+        val directions = CharacterListFragmentDirections.actionCharacterListFragmentToCharacterDetailFragment(
+            characterId = characterId
+        )
+        findNavController().navigate(directions)
     }
 }
